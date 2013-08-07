224d87590d4174f36c65198efd147074a80d15de
MaryHal <maryhal@users.noreply.github.com>
2013-08-07 03:23:30 -0700

    Ace-jump-mode configuration

--- config.el
+++ config.el
@@ -256,12 +256,12 @@
 (require 'helm-files)
 (set-face-attribute 'helm-selection nil
                     :background nil
-                    :foreground "brightwhite"
+                    :foreground "white"
                     :underline t)
 (set-face-attribute 'helm-source-header nil
                     :weight 'bold
                     :background "grey30"
-                    :foreground "brightwhite"
+                    :foreground "white"
                     :underline nil)
 (set-face-attribute 'helm-header nil
                     :weight 'bold
@@ -720,6 +720,11 @@ the current state and point position."
 
 ;; Ace Jump
 (require 'ace-jump-mode)
+(setq ace-jump-mode-case-fold t)
+(setq ace-jump-mode-scope 'window)
+
+(global-set-key (kbd "C-c SPC") 'ace-jump-char-mode)
+;; (define-key evil-normal-state-map (kbd "") 'ace-jump-mode)
 
 ;; Expand Region
 (require 'expand-region)
@@ -903,59 +908,3 @@ the current state and point position."
   (kbd "M-J") 'org-metadown
   (kbd "M-K") 'org-metaup
   (kbd "M-L") 'org-metaright)
-
-;; Ace Jump Motions
-;; (defmacro evil-enclose-ace-jump (&rest body)
-;;   `(let ((old-mark (mark)))
-;;      (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
-;;      (remove-hook 'post-command-hook #'evil-visual-post-command t)
-;;      (unwind-protect
-;;          (progn
-;;            ,@body
-;;            (recursive-edit))
-;;        (if (evil-visual-state-p)
-;;            (progn
-;;              (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
-;;              (add-hook 'post-command-hook #'evil-visual-post-command nil t)
-;;              (set-mark old-mark))
-;;          (push-mark old-mark)))))
-
-;; (evil-define-motion evil-ace-jump-char-mode (count)
-;;   :type exclusive
-;;   (evil-enclose-ace-jump
-;;    (ace-jump-mode 5)))
-
-;; (evil-define-motion evil-ace-jump-line-mode (count)
-;;   :type line
-;;   (evil-enclose-ace-jump
-;;    (ace-jump-mode 9)))
-
-;; (evil-define-motion evil-ace-jump-word-mode (count)
-;;   :type exclusive
-;;   (evil-enclose-ace-jump
-;;    (ace-jump-mode 1)))
-
-;; (evil-define-motion evil-ace-jump-char-to-mode (count)
-;;   :type exclusive
-;;   (evil-enclose-ace-jump
-;;    (ace-jump-mode 5)
-;;    (forward-char -1)))
-
-;; some proposals for binding:
-
-;; (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode)
-;;(define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-word-mode)
-;;
-;;(define-key evil-operator-state-map (kbd "SPC") #'evil-ace-jump-char-mode) ;; similar to f
-;;(define-key evil-operator-state-map (kbd "C-SPC") #'evil-ace-jump-char-to-mode) ;; similar to t
-;;(define-key evil-operator-state-map (kbd "M-SPC") #'evil-ace-jump-word-mode)
-
-;; different jumps for different visual modes
-;; (defadvice evil-visual-line (before spc-for-line-jump activate)
-;;   (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-line-mode))
-
-;; (defadvice evil-visual-char (before spc-for-char-jump activate)
-;;   (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
-
-;; (defadvice evil-visual-block (before spc-for-char-jump activate)
-;;   (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
