7b752157db83db136a3ac9cabf3e1b313a20bbc8
MaryHal <maryhal@users.noreply.github.com>
2013-10-10 17:57:18 -0700

    Smart-mode-line colors in GUI Emacs fixed, changed color theme

--- custom.el
+++ custom.el
@@ -3,11 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (base16-eighties)))
- '(custom-safe-themes (quote ("51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" default)))
+ '(custom-enabled-themes (quote (base16-default)))
+ '(custom-safe-themes (quote ("1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" default)))
  '(helm-candidate-number-limit nil)
  '(helm-quick-update t)
- '(sml/col-number-format " %4c"))
+ '(sml/active-background-color "color-235")
+ '(sml/col-number-format " %4c")
+ '(sml/inactive-background-color "color-233"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
