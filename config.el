9bd72d99d3e423bc474bc6d6e9eed45d359a6393
MaryHal <maryhal@users.noreply.github.com>
2013-11-16 14:03:35 -0800

    Cleaned up clang flags

--- config.el
+++ config.el
@@ -400,9 +400,9 @@
         (mapcar (lambda (item)(concat "-I" item))
                 (split-string
                  "
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/../../../../include/c++/4.8.2
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/../../../../include/c++/4.8.2/x86_64-unknown-linux-gnu
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/../../../../include/c++/4.8.2/backward
+ /usr/include/c++/4.8.2
+ /usr/include/c++/4.8.2/x86_64-unknown-linux-gnu
+ /usr/include/c++/4.8.2/backward
  /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/include
  /usr/local/include
  /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.2/include-fixed
