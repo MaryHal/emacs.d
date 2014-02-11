0d7ed865c9ced03a8e45d216880ffa631b76b567
MaryHal <maryhal@users.noreply.github.com>
2014-02-11 12:50:30 -0800

    js2-mode by default in javascript files

--- config.el
+++ config.el
@@ -499,6 +499,11 @@
 (add-to-list 'auto-mode-alist '("\\.vm$" . html-mode))
 
 ;; Javascript
+(require 'js2-mode)
+(setq js2-highlight-level 3)
+;; (setq-default js2-basic-offset 2)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
 
