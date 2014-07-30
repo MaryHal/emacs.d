3c4e4d526b74bed1e01cc82156db23443a5d4751
MaryHal <maryhal@users.noreply.github.com>
2014-07-29 17:13:40 -0700

    One file. One big file.

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "c1fb68aa00235766461c7e31ecfc759aa2dd905899ae6d95097061faeb72f9ee" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" default)))
- ;; '(sml/active-background-color "gray10")
- ;; '(sml/inactive-background-color "gray18")
+ '(company-idle-delay 0.1)
+ '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "c1fb68aa00235766461c7e31ecfc759aa2dd905899ae6d95097061faeb72f9ee" "7feeed063855b06836e0262f77f5c6d3f415159a98a9676d549bfeb6c49637c4" default)))
  '(sml/col-number-format "%3c")
  '(sml/line-number-format " %3l"))
 (custom-set-faces
@@ -13,10 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(header-line ((t (:inherit mode-line :inverse-video nil))))
- ;; '(isearch-fail ((t (:background "red" :foreground "brightwhite"))))
- '(lazy-highlight ((t (:background "#005F87" :foreground "#EEEEEE"))))
- '(region ((t (:background "#005F87" :foreground "#EEEEEE"))))
- '(mode-line ((t (:background "gray6" :foreground "gray60"))))
- '(mode-line-inactive ((t (:background "gray10" :foreground "gray60"))))
  '(trailing-whitespace ((t (:background "goldenrod")))))
