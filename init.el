3d65da3a2c4d68bd2529f2f8a209874f94bbd0c4
MaryHal <maryhal@users.noreply.github.com>
2014-10-15 22:01:22 -0700

    Consistent Comments

--- init.el
+++ init.el
@@ -178,7 +178,7 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward
       uniquify-separator "/"
-      uniquify-ignore-buffers-re "^\\*" ; leave special buffers alone
+      uniquify-ignore-buffers-re "^\\*" ;; leave special buffers alone
       uniquify-after-kill-buffer-p t)
 
 ;; A saner ediff
@@ -573,15 +573,15 @@
 (setq wg-prefix-key (kbd "C-c z"))
 
 ;; What to do on Emacs exit / workgroups-mode exit?
-(setq wg-emacs-exit-save-behavior           nil)      ; Options: 'save 'ask nil
-(setq wg-workgroups-mode-exit-save-behavior nil)      ; Options: 'save 'ask nil
+(setq wg-emacs-exit-save-behavior           nil)      ;; Options: 'save 'ask nil
+(setq wg-workgroups-mode-exit-save-behavior nil)      ;; Options: 'save 'ask nil
 
 ;; Mode Line changes
 ;; Display workgroups in Mode Line?
-(setq wg-mode-line-display-on t)          ; Default: (not (featurep 'powerline))
-(setq wg-flag-modified t)                 ; Display modified flags as well
+(setq wg-mode-line-display-on t)          ;; Default: (not (featurep 'powerline))
+(setq wg-flag-modified t)                 ;; Display modified flags as well
 (setq wg-mode-line-decor-left-brace "["
-      wg-mode-line-decor-right-brace "]"  ; how to surround it
+      wg-mode-line-decor-right-brace "]"  ;; how to surround it
       wg-mode-line-decor-divider ":")
 
 (workgroups-mode t)
