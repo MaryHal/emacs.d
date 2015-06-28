3b2584134dd4712e0b63fc46d9f0c63a15c3b134
MaryHal <maryhal@users.noreply.github.com>
2015-06-27 22:40:06 -0700

    Multiple-cursors hydra

--- init.el
+++ init.el
@@ -1020,6 +1020,24 @@ If region is active, apply to active region instead."
                               ;; ("L" hydra-move-splitter-right)
 
                               ("q" nil)))
+
+
+          (bind-key "C-c n" (defhydra cqql-multiple-cursors-hydra (:hint nil)
+                              "
+^Up^            ^Down^        ^Miscellaneous^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_q_] Quit "
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("q" nil)))
           ))
 
 
