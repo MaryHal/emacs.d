4aaf48400b389f401b2898d01ec26d814c146ed2
MaryHal <maryhal@users.noreply.github.com>
2017-07-25 12:53:41 -0700

    Windows install creates symlink

--- WindowsInstall.bat
+++ WindowsInstall.bat
@@ -1,4 +1,5 @@
-rmdir /S "%USERPROFILE%\AppData\Roaming\.emacs.d\"
 mkdir "%USERPROFILE%\AppData\Roaming\.emacs.d\"
 mkdir "%USERPROFILE%\AppData\Roaming\.emacs.d\cache\"
+mkdir "%USERPROFILE%\AppData\Roaming\.emacs.d\server\"
 xcopy /E /Y * "%USERPROFILE%\AppData\Roaming\.emacs.d\"
+mklink /D "%USERPROFILE%\.emacs.d" "%USERPROFILE%\AppData\Roaming\.emacs.d"
