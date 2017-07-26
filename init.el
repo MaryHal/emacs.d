89197c0ba5d793c8048db415ebf583d0a1da26fe
MaryHal <maryhal@users.noreply.github.com>
2017-07-25 18:58:19 -0700

    Tried a few things to fullscreen magit-status

--- init.el
+++ init.el
@@ -1309,6 +1309,8 @@ a terminal, just try to remove default the background color."
   :config (progn
             (if (string= system-type "windows-nt")
                 (setenv "SSH_ASKPASS" "git-gui--askpass"))
+
+            ;; (setq-default magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
             ))
 
 (use-package git-timemachine
@@ -2142,17 +2144,17 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   :config (progn
             (setq shackle-rules
                   '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :size 0.4)
-                    ;; ("^\\*magit"   :regexp t :align t :size 0.4)
+                    ;; ("^\\*magit[^*]+$" :regexp t :same t :noselect t)
+                    ;; ("COMMIT_EDITMSG" :align t :popup t)
+                    ;; ("^\\*magit.*popup\\*$"  :regexp t :align t :size 0.4)
                     ("*Backtrace*" :size 20  :align t :noselect t)
                     ("*Warnings*"  :size 8   :align t :noselect t)
                     ("*Messages*"  :size 12  :align t :noselect t)
                     ("*Help*"      :size 0.3 :align t)
                     ("^\\*.*Shell Command.*\\*$" :regexp t :align t :size 20 :noselect t)
                     (apropos-mode :size 0.3 :align t)
-                    (compilation-mode :align t :align 0.4)))
-
-            (setq shackle-default-rule '(:select t))
-               ))
+                    (compilation-mode :align t :size 0.4)))
+            (setq shackle-default-rule '(:select t))))
 
 ;; Dired ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
