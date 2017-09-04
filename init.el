cf69df54198344dfd28d7a8af3cbfdda520d5ccf
MaryHal <maryhal@users.noreply.github.com>
2017-09-03 22:24:17 -0700

    Prefer unix line endings

--- init.el
+++ init.el
@@ -266,13 +266,13 @@ selection of all minor-modes, active or not."
     (setq-default buffer-file-coding-system 'utf-8)
   (setq buffer-file-coding-system 'utf-8-unix))
 
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system       'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
 
-(setq coding-system-for-read 'utf-8)
-(setq coding-system-for-write 'utf-8)
+(setq coding-system-for-read 'utf-8-unix)
+(setq coding-system-for-write 'utf-8-unix)
 
 ;; Enable syntax highlighting for older Emacsen that have it off
 (global-font-lock-mode t)
